1e0cda636562a02c5a3d9df63c67877769d735f5
Leonardo <leonardo.fuentes@gmail.com>
2020-12-08 12:11:40 -0300

    Corrige estado de la respuesta

--- glogic/src/main/java/com/lfuentes/glogic/api/UsuarioApi.java
+++ glogic/src/main/java/com/lfuentes/glogic/api/UsuarioApi.java
@@ -6,6 +6,7 @@ import org.dozer.Mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,6 +43,6 @@ public class UsuarioApi {
 		RegistroResponse respuesta = mapper.map(usuarioRespuesta, RegistroResponse.class);
 		
 		logger.info("registro[FIN] respuesta.Id: "+ respuesta.getId());
-		return ResponseEntity.ok(respuesta);
+		return new ResponseEntity <>(respuesta,HttpStatus.CREATED );
 	}
 }
