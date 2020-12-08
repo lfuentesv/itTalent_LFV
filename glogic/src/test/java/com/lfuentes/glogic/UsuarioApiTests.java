6a4a83cbeffa4ce786ee742de0bfc818a0adf344
Leonardo <leonardo.fuentes@gmail.com>
2020-12-08 12:48:38 -0300

    Agrega prueba para búsqueda por id

--- glogic/src/test/java/com/lfuentes/glogic/UsuarioApiTests.java
+++ glogic/src/test/java/com/lfuentes/glogic/UsuarioApiTests.java
@@ -1,6 +1,7 @@
 package com.lfuentes.glogic;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lfuentes.glogic.dao.UsuarioRepositorio;
 import com.lfuentes.glogic.dto.RegistroRequest;
 import com.lfuentes.glogic.dto.Telefono;
+import com.lfuentes.glogic.dto.Usuario;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -125,6 +128,35 @@ public class UsuarioApiTests {
 				   .andExpect(jsonPath("$.errores[0].mensaje").value("El correo debe cumplir con el formato: aaaaaaa@dominio.cl"))
 				   .andExpect(status().isBadRequest());
 
+	}
+	
+	@Test
+	void buscarUsuarioId() throws Exception {
+		Usuario usuario = new Usuario();
+		usuario.setName("Miguel Rodriguez");
+		usuario.setEmail("miky@rodriguez.cl");
+		usuario.setPassword("Hunter22");
+		Telefono telefono = new Telefono();
+		telefono.setNumber((long) 1234567);
+		telefono.setCitycode(1);
+		telefono.setContrycode(57);
+		usuario.setPhones(Arrays.asList(telefono));
+
+		Usuario nuevoUsuario = repo.saveAndFlush(usuario);
+
+		mockMvc.perform(get("/usuario/{id}",nuevoUsuario.getId())
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("$.id").value(nuevoUsuario.getId()))
+					.andExpect(jsonPath("$.name").value(usuario.getName()))
+					.andExpect(jsonPath("$.email").value(usuario.getEmail()))
+					.andExpect(jsonPath("$.password").value(usuario.getPassword()))
+					.andExpect(jsonPath("$.created").isNotEmpty())
+					.andExpect(jsonPath("$.modified").isEmpty())
+					.andExpect(jsonPath("$.last_login").isNotEmpty())
+					.andExpect(jsonPath("$.isactive").isNotEmpty())
+					;
+		
 	}
 
 }
