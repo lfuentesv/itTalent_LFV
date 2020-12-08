37a8b1c8fe1533803e5463dcdd4a9dca9cba6e5c
Leonardo <leonardo.fuentes@gmail.com>
2020-12-08 03:32:51 -0300

    Agrega test email duplicado

--- glogic/src/test/java/com/lfuentes/glogic/UsuarioApiTests.java
+++ glogic/src/test/java/com/lfuentes/glogic/UsuarioApiTests.java
@@ -2,6 +2,7 @@ package com.lfuentes.glogic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
@@ -51,6 +52,31 @@ public class UsuarioApiTests {
 			   .andExpect(status().isOk());
 
 		assertThat(repo.existsUsuarioByEmail("juan@rodriguez.cl"));
+	}
+	
+	@Test
+	void emailDuplicado() throws Exception {
+		RegistroRequest usuario = new RegistroRequest();
+		usuario.setName("Pedro Rodriguez");
+		usuario.setEmail("pedro@rodriguez.cl");
+		usuario.setPassword("Hunter22");
+		Telefono telefono = new Telefono();
+		telefono.setNumber((long) 1234567);
+		telefono.setCitycode(1);
+		telefono.setContrycode(57);
+		usuario.setPhones(Arrays.asList(telefono));
+
+		mockMvc.perform(post("/usuario")
+			   .contentType("application/json")
+			   .content(objectMapper.writeValueAsString(usuario)))
+			   .andExpect(status().isOk());
+		
+		mockMvc.perform(post("/usuario")
+				   .contentType("application/json")
+				   .content(objectMapper.writeValueAsString(usuario)))
+				   .andExpect(jsonPath("$.errores[0].mensaje").value("El correo informado ya esta registrado (pedro@rodriguez.cl)"))
+				   .andExpect(status().isBadRequest());
+
 	}
 
 }
