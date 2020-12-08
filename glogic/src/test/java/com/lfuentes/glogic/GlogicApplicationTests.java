b80dac278201c27b79d4857e397f82297363ecec
Leonardo <leonardo.fuentes@gmail.com>
2020-12-07 22:00:59 -0300

    Se crea el proyecto gradle

---
+++ glogic/src/test/java/com/lfuentes/glogic/GlogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfuentes.glogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
