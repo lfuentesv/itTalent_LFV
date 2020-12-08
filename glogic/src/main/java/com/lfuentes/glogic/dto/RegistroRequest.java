09ffa9ece0fe3460a183c7d35293670ad83cefff
Leonardo <leonardo.fuentes@gmail.com>
2020-12-08 01:56:12 -0300

    Agrega validaciones de campos

--- glogic/src/main/java/com/lfuentes/glogic/dto/RegistroRequest.java
+++ glogic/src/main/java/com/lfuentes/glogic/dto/RegistroRequest.java
@@ -2,21 +2,17 @@ package com.lfuentes.glogic.dto;
 
 import java.util.List;
 
+import javax.validation.constraints.Pattern;
+
 public class RegistroRequest {
 
 	private String name;
+	@Pattern(regexp = "^[a-z]{1,7}@([\\w-]+\\.)+cl$" , message="El correo debe cumplir con el formato: aaaaaaa@dominio.cl")
 	private String email;
+	@Pattern(regexp = "^[A-Z]{1}[a-z]*[0-9]{2}$" , message="La contraseña debe cumplir con el formato: Una mayúscula, letras minúsculas y dos números")
 	private String password;
 	private List<Telefono> phones;
 	
-	public List<Telefono> getPhones() {
-		return phones;
-	}
-
-	public void setPhones(List<Telefono> phones) {
-		this.phones = phones;
-	}
-
 	public RegistroRequest() {
 	}
 
@@ -43,5 +39,15 @@ public class RegistroRequest {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public List<Telefono> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(List<Telefono> phones) {
+		this.phones = phones;
+	}
+	
+	
 	
 }
