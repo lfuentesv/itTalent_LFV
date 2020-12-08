0d48548bb0d1a2f1517bdcf065fb8953a38a9eb0
Leonardo <leonardo.fuentes@gmail.com>
2020-12-07 22:14:59 -0300

    Agrega controlador básico

--- glogic/src/main/java/com/lfuentes/glogic/api/UsuarioApi.java
+++ glogic/src/main/java/com/lfuentes/glogic/api/UsuarioApi.java
@@ -1,5 +1,18 @@
 package com.lfuentes.glogic.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
 public class UsuarioApi {
 
+	@PostMapping("")
+	public ResponseEntity<String> registro(@RequestBody String usuario){
+		
+		return ResponseEntity.ok("registro");
+	}
 }
