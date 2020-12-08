2af851cdbffebea834d759d9f8823dff1d29b09c
Leonardo <leonardo.fuentes@gmail.com>
2020-12-07 23:53:02 -0300

    Actualiza repositorio

--- glogic/src/main/java/com/lfuentes/glogic/dao/UsuarioRepositorio.java
+++ glogic/src/main/java/com/lfuentes/glogic/dao/UsuarioRepositorio.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lfuentes.glogic.dto.Usuario;
 
-public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepositorio extends JpaRepository<Usuario, String> {
 
 }
