2ba43dde148693403d028a456d952976d02adf7e
Leonardo <leonardo.fuentes@gmail.com>
2020-12-08 02:17:14 -0300

    Agrega advisor con manejo de errores en campos

---
+++ glogic/src/main/java/com/lfuentes/glogic/validacion/RegistroControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.lfuentes.glogic.validacion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.lfuentes.glogic.dto.Error;
+import com.lfuentes.glogic.dto.ErrorResponse;
+
+
+@ControllerAdvice
+public class RegistroControllerAdvice {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	ErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		ErrorResponse respuestaError = new ErrorResponse();
+		for (FieldError campo : e.getBindingResult().getFieldErrors()) {
+			respuestaError.getErrores().add(new Error(campo.getDefaultMessage()));
+		}
+		return respuestaError;
+	}
+}
