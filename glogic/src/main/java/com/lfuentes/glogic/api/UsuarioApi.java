6ae78aa64e613ca36632dc80c34f4a202dfccaaf
Leonardo <leonardo.fuentes@gmail.com>
2020-12-07 22:31:25 -0300

    Actualiza parámetro del servicio e incorpora Log

--- glogic/src/main/java/com/lfuentes/glogic/api/UsuarioApi.java
+++ glogic/src/main/java/com/lfuentes/glogic/api/UsuarioApi.java
@@ -1,18 +1,27 @@
 package com.lfuentes.glogic.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lfuentes.glogic.dto.Usuario;
+
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioApi {
 
+	Logger logger = LoggerFactory.getLogger(UsuarioApi.class);
+	
 	@PostMapping("")
-	public ResponseEntity<String> registro(@RequestBody String usuario){
+	public ResponseEntity<String> registro(@RequestBody Usuario usuario){
+		logger.info("registro[INI] Usuario.name: "+ usuario.getName());
 		
+		
+		logger.info("registro[FIN]");
 		return ResponseEntity.ok("registro");
 	}
 }
