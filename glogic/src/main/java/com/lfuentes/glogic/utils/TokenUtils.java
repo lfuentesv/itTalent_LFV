994a37e09e7708086c962b26c7f39dda585aad32
Leonardo <leonardo.fuentes@gmail.com>
2020-12-08 00:52:37 -0300

    Agrega log

--- glogic/src/main/java/com/lfuentes/glogic/utils/TokenUtils.java
+++ glogic/src/main/java/com/lfuentes/glogic/utils/TokenUtils.java
@@ -3,6 +3,8 @@ package com.lfuentes.glogic.utils;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -11,6 +13,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class TokenUtils implements Serializable{
 
+	private static Logger logger = LoggerFactory.getLogger(TokenUtils.class);
+	
 	private static final long serialVersionUID = 7450588564010985148L;
 
 	private static String secret = "secreto";
@@ -18,7 +22,7 @@ public class TokenUtils implements Serializable{
 	public static final long JWT_TOKEN_DURACION = 2 * 60 * 60 * 10000; //2H
 	
 	public static String getJWTToken(String nombreUsuario ) {
-	
+		logger.info("getJWTToken[INI] nombreUsuario: "+nombreUsuario);
 		String token = Jwts
 				.builder()
 				.setId("lfv")
@@ -27,6 +31,7 @@ public class TokenUtils implements Serializable{
 				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_DURACION))
 				.signWith(SignatureAlgorithm.HS512,secret.getBytes()).compact();
 
+		logger.info("getJWTToken[FIN] token: "+token);
 		return token;
 	}
 	
