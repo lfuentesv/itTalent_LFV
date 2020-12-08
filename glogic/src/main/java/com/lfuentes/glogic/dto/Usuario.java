1bca1190607ace5af0d8452d73802af93ca948fa
Leonardo <leonardo.fuentes@gmail.com>
2020-12-07 22:23:47 -0300

    Agrega dto, corrige endpoint

---
+++ glogic/src/main/java/com/lfuentes/glogic/dto/Usuario.java
@@ -0,0 +1,47 @@
+package com.lfuentes.glogic.dto;
+
+import java.util.List;
+
+public class Usuario {
+	
+	private String name;
+	private String email;
+	private String password;
+	private List<Telefono> telefonos;
+	
+	public Usuario() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Telefono> getTelefonos() {
+		return telefonos;
+	}
+
+	public void setTelefonos(List<Telefono> telefonos) {
+		this.telefonos = telefonos;
+	}
+
+}
