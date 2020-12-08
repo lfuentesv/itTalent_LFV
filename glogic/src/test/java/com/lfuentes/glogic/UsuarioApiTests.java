966137f9d6c022d300e4c9c59b6f04d1c6141f00
Leonardo <leonardo.fuentes@gmail.com>
2020-12-08 03:23:17 -0300

    Agrega prueba de registro contra Api

---
+++ glogic/src/test/java/com/lfuentes/glogic/UsuarioApiTests.java
@@ -0,0 +1,56 @@
+package com.lfuentes.glogic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lfuentes.glogic.dao.UsuarioRepositorio;
+import com.lfuentes.glogic.dto.RegistroRequest;
+import com.lfuentes.glogic.dto.Telefono;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsuarioApiTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private UsuarioRepositorio repo;
+
+	@Test
+	void registarUsuario() throws Exception {
+		RegistroRequest usuario = new RegistroRequest();
+		usuario.setName("Juan Rodriguez");
+		usuario.setEmail("juan@rodriguez.cl");
+		usuario.setPassword("Hunter22");
+		Telefono telefono = new Telefono();
+		telefono.setNumber((long) 1234567);
+		telefono.setCitycode(1);
+		telefono.setContrycode(57);
+		usuario.setPhones(Arrays.asList(telefono));
+
+		mockMvc.perform(post("/usuario")
+			   .contentType("application/json")
+			   .content(objectMapper.writeValueAsString(usuario)))
+			   .andExpect(status().isOk());
+
+		assertThat(repo.existsUsuarioByEmail("juan@rodriguez.cl"));
+	}
+
+}
